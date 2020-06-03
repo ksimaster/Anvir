74b51277eb6b42baf150f68b6a6f0bfed516fd6a
ksimaster <ksimasterlord@gmail.com>
2020-06-03 10:51:37 +0300

    Создание рыбы
    Создание работоспособной рыбы кликера

---
+++ Anvir/Assets/Scripts/ClickScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickScript : MonoBehaviour
+{
+    public int score;
+    public Text clickText;
+    void Start()
+    {
+        score = 0;
+        score = PlayerPrefs.GetInt("Score+", score);
+    }
+
+   
+    void Update()
+    {
+        clickText.text = score.ToString();
+    }
+
+    public void ClikerScore()
+    {
+        score++;
+        PlayerPrefs.SetInt("Score+", score);
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        score = 0;
+    }
+}

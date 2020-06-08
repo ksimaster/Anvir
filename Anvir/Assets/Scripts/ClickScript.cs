84e98ec5d0c9ec022c1e561aea4d700d6efaa586
ksimaster <ksimasterlord@gmail.com>
2020-06-08 20:08:19 +0300

    Добавлен функционал вируса
    Добавлен функционал вируса

--- Anvir/Assets/Scripts/ClickScript.cs
+++ Anvir/Assets/Scripts/ClickScript.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class ClickScript : MonoBehaviour
 {
     public int score;
     public Text clickText;
+
+    public int virusScore;
+    public Text virusText;
+
+    public float speedVirus;
     void Start()
     {
         score = 0;
         score = PlayerPrefs.GetInt("Score+", score);
+        virusScore = 0;
+        virusScore = PlayerPrefs.GetInt("VirusScore+", virusScore);
+        StartCoroutine("UpVirus");
     }
 
    
     void Update()
     {
         clickText.text = score.ToString();
+        virusText.text = virusScore.ToString();
+        //if (virusScore < 0) virusScore++;
+
     }
 
+    
     public void ClikerScore()
     {
         score++;
@@ -29,5 +42,21 @@ public class ClickScript : MonoBehaviour
     {
         PlayerPrefs.DeleteAll();
         score = 0;
+        virusScore = 0;
     }
+
+    IEnumerator UpVirus()
+    {
+        while (score < 1000) { 
+            virusScore = virusScore + Random.Range(-2, 4);
+            
+            PlayerPrefs.SetInt("VirusScore+", virusScore);
+            virusText.text = virusScore.ToString();
+            if (virusScore <= 0) virusScore=1;
+            speedVirus = (2.0f / virusScore) + 0.0006f * score;
+            yield return new WaitForSeconds(speedVirus);
+        }
+    }
+
+    
 }
